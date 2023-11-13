a5096d4b35fb471df2e67b026b1b2ed1df549458
MarissaSmith <missmars1@gmail.com>
2023-11-13 16:46:14 -0600

    Update package files and App.js

--- src/App.js
+++ src/App.js
@@ -2,28 +2,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Counter from './components/Counter';  // Import your connected component
+import Counter from './components/Counter'; // Import your connected component
+import { Container, Navbar, Nav } from 'react-bootstrap'; // Import Bootstrap components
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      {/* Use your connected component */}
-      <Counter />
-    </div>
+    <Container fluid>
+      {/* Navbar with Bootstrap styling */}
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Your React App</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+      </Navbar>
+
+      {/* Main content container */}
+      <Container>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+
+        {/* Use your connected component */}
+        <Counter />
+      </Container>
+    </Container>
   );
 }
 
